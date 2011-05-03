05aac17115893357d66a3fd5e7c7eeb3af5af7b8
Simon Baumgartner <makesens3@gmail.com>
2011-05-03 12:17:01 -0400

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  
+  def new    
+  end
+  
+  # sessions_controller.rb
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
+  
+  def failure
+    render :text => "ERROR: #{params[:message]}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThinOpenidClient::Application.config.secret_token = '5b581b57484ecfe2f8924bd0614110d51019a6e3f82596911628b44ba7d747f00ab0b313568929a07c7c2153a2f78070c87833159b177662a46eea12902c4215'
